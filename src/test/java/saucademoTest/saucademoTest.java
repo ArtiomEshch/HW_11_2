6898ad576d988b39dec47e250269badd90b79ca7
Ärtiom Yeshchenko <eshchenko@gmail.com>
2022-07-09 08:03:11 +0400

    HW 13

---
+++ src/test/java/saucademoTest/saucademoTest.java
@@ -0,0 +1,58 @@
+package saucademoTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class saucademoTest {
+    public static WebDriver driver;
+
+    @BeforeAll
+    public static void setup () {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Test
+    public void authorizationTest (){
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        assertTrue(driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).isDisplayed());
+    }
+
+    @Test
+    public void purchaseTest (){
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt")).click();
+
+        driver.findElement(By.id("shopping_cart_container")).click();
+
+        driver.findElement(By.id("checkout")).click();
+
+        driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys("UserFirstName");
+        driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys("UserLastName");
+        driver.findElement(By.xpath("//input[@id='postal-code']")).sendKeys("111111");
+
+        driver.findElement(By.id("continue")).click();
+
+        driver.findElement(By.id("finish")).click();
+        assertTrue(driver.findElement(By.id("checkout_complete_container")).isDisplayed());
+    }
+
+    @AfterAll
+    public static void finish (){
+        driver.quit();
+    }
+}
